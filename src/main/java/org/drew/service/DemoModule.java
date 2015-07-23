b7daea6a0342eae12f7f228ee80c8ed5422d6cdb
James Drew <j.drew1303@gmail.com>
2015-07-23 22:01:08 +0100

    injecting in metrics registry

--- src/main/java/org/drew/service/DemoModule.java
+++ src/main/java/org/drew/service/DemoModule.java
@@ -1,8 +1,11 @@
 package org.drew.service;
 
 import com.bendb.dropwizard.jooq.JooqBundle;
+import com.codahale.metrics.MetricRegistry;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import io.dropwizard.setup.Environment;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jooq.Configuration;
 import org.jooq.example.flyway.db.h2.flyway_test.tables.daos.BookDao;
@@ -11,13 +14,10 @@ import org.jooq.example.flyway.db.h2.flyway_test.tables.daos.BookDao;
  * Created by jamesdrew on 04/05/2015.
  */
 @Slf4j
+@AllArgsConstructor
 public class DemoModule extends AbstractModule {
 
     private final JooqBundle jooqBundle;
-
-    public DemoModule(JooqBundle jooqBundle) {
-        this.jooqBundle = jooqBundle;
-    }
 
     @Override
     protected void configure() {}
@@ -25,5 +25,10 @@ public class DemoModule extends AbstractModule {
     @Provides
     BookDao providesBookDao(){
         return new BookDao(jooqBundle.getConfiguration());
+    }
+
+    @Provides
+    MetricRegistry providesMetricRegistry(Environment environment){
+        return environment.metrics();
     }
 }
